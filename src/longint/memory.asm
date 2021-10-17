75e18e8f62a674c9a24a6bc5f6bd8f62a87d558f
galchapman <galchapman@gmail.com>
2021-10-18 00:06:27 +0300

    document memory functions

--- src/longint/memory.asm
+++ src/longint/memory.asm
@@ -7,6 +7,7 @@ global _reallocLongInt
 extern malloc
 extern realloc
 
+; create new long int
 newLongInt:
 	sub rsp, 40
 	mov ecx, 8
@@ -14,7 +15,8 @@ newLongInt:
 	mov qword[rax], 0
 	add rsp, 40
 	ret
-
+; allocate new long int vector
+; @rcx: size of buffer (in qwords)
 _allocLongInt:
 	sub rsp, 16
 	sal rcx, 3 ; arg *= sizeof(uint64_t)
@@ -23,7 +25,9 @@ _allocLongInt:
 	add rsp, 16
 	mov qword[rax], 0
 	ret
-
+; reallocate long int vector
+; @rcx: vector (in qwords)
+; @rdx: size of vector (in qwords)
 _reallocLongInt:
 	sub rsp, 32
 	mov rbx, rdx
